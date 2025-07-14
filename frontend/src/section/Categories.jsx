9121f55def930aaf3cd50363314d5bbc1c002edf
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-07-14 14:58:11 +0530

    feat: update background classes and adjust image size in Categories component

--- frontend/src/section/Categories.jsx
+++ frontend/src/section/Categories.jsx
@@ -8,17 +8,17 @@ const Categories = () => {
       {categories.map((category, index) => {
         let bgClass = "";
         if (category.name == "Breakfast") {
-          bgClass = "bg-[#FCE8D5]";
+          bgClass = "bg-[#FCE8D5]/40";
         } else if (category.name == "Chocolate") {
-          bgClass = "bg-[#FDE8E5]";
+          bgClass = "bg-[#FDE8E5]/40";
         } else if (category.name == "Dessert") {
-          bgClass = "bg-[#F8D8E4]";
+          bgClass = "bg-[#F8D8E4]/40";
         } else if (category.name == "Lunch") {
-          bgClass = "bg-[#E8F9FD]";
+          bgClass = "bg-[#E8F9FD]/40";
         } else if (category.name == "Meat") {
-          bgClass = "bg-[#FCE8D5]";
+          bgClass = "bg-[#FCE8D5]/40";
         } else if (category.name == "Vegan") {
-          bgClass = "bg-[#E8F9FD]";
+          bgClass = "bg-[#E8F9FD]/40";
         }
         return (
           <div
@@ -26,10 +26,10 @@ const Categories = () => {
   className={`relative flex flex-col items-center justify-center gap-8 p-6 cursor-pointer hover:scale-105 transition-transform duration-300 rounded-2xl ${bgClass}`}
 >
   {/* Glass effect at top */}
-  <div className="absolute top-0 left-0 w-full h-full rounded-t-2xl backdrop-blur-3xl bg-white/50 pointer-events-none"></div>
+  <div className="absolute top-0 left-0 w-full h-16 rounded-t-2xl backdrop-blur-md bg-white/30 pointer-events-none"></div>
   <img
     src={category.image}
-    className="w-[100px] h-[100px] object-cover z-10"
+    className="w-[75px] h-[75px] object-cover z-10"
     alt=""
   />
   <p className="font-bold z-10">{category.name}</p>
