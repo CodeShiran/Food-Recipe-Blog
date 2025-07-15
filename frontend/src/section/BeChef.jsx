e250812a5ece31bdf7f796d0d4233a0368dd4a4c
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-07-15 16:38:41 +0530

    feat: enhance BeChef component layout with responsive design elements

--- frontend/src/section/BeChef.jsx
+++ frontend/src/section/BeChef.jsx
@@ -9,7 +9,11 @@ const BeChef = () => {
         <p className='text-gray-400 mt-[50px]'>Discover the joy of cooking with our easy-to-follow recipes and tips.</p>
         <button className='mt-[50px] bg-black text-white px-4 py-2 rounded-3xl hover:bg-gray-800 transition-all duration-150 hover:shadow-2xl cursor-pointer'>Learn More</button>
       </div>
-      <div className='flex-1 items-center overflow-hidden'> 
+      <div className='relative flex-1 items-center overflow-hidden'> 
+        <div className='block md:hidden absolute inset-0  bg-black/50'></div>
+        <div className=' md:hidden absolute inset-0 flex items-center justify-center ml-4'>
+          <h2 className='text-5xl font-bold text-white'>Everyone can be a <br />chef in their own kitchen</h2>
+        </div>
         <img src={assets.chef} alt="" />
       </div>
     </div>
