bf10adb8f580481e2eb5020cb11b14672f68f9b4
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-07-15 22:23:55 +0530

    feat: add new background images for Egg Salad, Leaves, and Vegan Salad

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,12 +1,18 @@
 import Hero from './hero.png'
 import Recipes from './recipes.png'
 import Chef from './chef.png'
+import EggSaladBg from './eggsaladbg.png'
+import LeavesBg from './leavesbg.png'
+import VeganSaladBg from './vegesaladbg.png'
 
 
 export const assets = {
   hero: Hero,
   recipes: Recipes,
-  chef: Chef
+  chef: Chef,
+  eggSaladBg: EggSaladBg,
+  leavesBg: LeavesBg,
+  veganSaladBg: VeganSaladBg
   // Add more assets here
 }
 
