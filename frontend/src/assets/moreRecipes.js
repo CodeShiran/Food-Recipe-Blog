c5183a3c8049bc0385cdb10322f16b5bf0bca82c
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-07-15 19:17:18 +0530

    feat: add new recipe images and moreRecipes data module

---
+++ frontend/src/assets/moreRecipes.js
@@ -0,0 +1,60 @@
+import BbqSandwitch from './bbqsandwitch.png'
+import BeefChickenBurger from './beefcheeseburger.png'
+import ChickenRamen from './chickenramensoup.png'
+import ChickenSalad from './chickensalad.png'
+import JapaneseFriedRice from './japanesefriedrice.png'
+import MixedFruitSalad from './mixedfruitsalad.png'
+import VeganLettuce from './veganlettuce.png'
+import VegetarianTaco from './vegetariantaco.png'
+
+const moreRecipes = [{
+    name: 'Barbeque Spicy Sandwiches with Chips  ',
+    image: BbqSandwitch,
+    time: '25 minutes',
+    type: 'Chicken'
+  },
+  {
+    name: 'Big and Juicy Wagyu Beef Cheeseburger',
+    image: BeefChickenBurger,
+    time: '30 minutes',
+    type: 'Beef'
+  },
+  {
+    name: 'Chicken Ramen Soup with Mushroom ',
+    image: ChickenRamen,
+    time: '35 minutes',
+    type: 'Chicken'
+  },
+  {
+    name: 'Rainbow Chicken Salad with Almond Honey Mustard Dressing',
+    image: ChickenSalad,
+    time: '20 minutes',
+    type: 'Salad'
+  },
+  {
+    name: 'Healthy Japanese Fried Rice with Asparagus',
+    image: JapaneseFriedRice,
+    time: '30 minutes',
+    type: 'Vegetarian'
+  },
+  {
+    name: 'Mixed Tropical Fruit Salad with Superfood Boosts ',
+    image: MixedFruitSalad,
+    time: '15 minutes',
+    type: 'Salad'
+  },
+  {
+    name: 'Firecracker Vegan Lettuce Wraps - Spicy! ',
+    image: VeganLettuce,
+    time: '20 minutes',
+    type: 'Vegan'
+  },
+  {
+    name: 'Cauliflower Walnut Vegetarian Taco Meat',
+    image: VegetarianTaco,
+    time: '25 minutes',
+    type: 'Vegetarian'
+}]
+
+
+export default moreRecipes;
