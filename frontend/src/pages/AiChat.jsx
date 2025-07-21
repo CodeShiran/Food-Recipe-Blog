922a0455294ac003f71582569abd15649140043e
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-07-21 14:32:07 +0530

    feat: enhance AiChat component with layout and input functionality

--- frontend/src/pages/AiChat.jsx
+++ frontend/src/pages/AiChat.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { FaCircleArrowUp } from "react-icons/fa6";
 
 const AiChat = () => {
   return (
-    <div>
-        
+    <div className='md:px-[50px] px-[25px] py-[150px]'>
+        <div className='mt-[100px] text-center'>
+            <h1 className='text-4xl font-bold'>AI Chat</h1>
+        </div>
+        <div className='mt-[50px] flex flex-row items-center justify-between max-w-[600px] mx-auto p-4 border-2 border-gray-300 rounded-lg shadow-md bg-white'>
+            <div className='w-full '>
+                <input className='p-2 focus:outline-none' type="text" placeholder='Type your message...' />
+            </div>
+            <button>
+                <FaCircleArrowUp className='text-2xl ' />
+            </button>
+        </div>
     </div>
   )
 }
