96943c55754ee4d1038d5ee0f78b2b0d7e29f522
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-07-25 19:52:00 +0530

    feat: implement search functionality in Recipes component to filter recipes by name

--- frontend/src/pages/Recipes.jsx
+++ frontend/src/pages/Recipes.jsx
@@ -3,14 +3,25 @@ import moreRecipes from "../assets/moreRecipes";
 import RecipeCard from "../components/RecipeCard";
 
 const Recipes = () => {
+
+    const [search, setSearch] = useState("");
+
+    const filteredPages = moreRecipes.filter(recipe =>
+        recipe.name.toLowerCase().includes(search.toLowerCase())
+    );
+
   const [currentPage, setCurrentPage] = useState(1);
   const recipesPerPage = 8;
 
   const startIndex = (currentPage - 1) * recipesPerPage;
   const endIndex = startIndex + recipesPerPage;
-  const recipesToShow = moreRecipes.slice(startIndex, endIndex);
+  const recipesToShow = filteredPages.slice(startIndex, endIndex);
 
-  const totalPages = Math.ceil(moreRecipes.length / recipesPerPage);
+  const totalPages = Math.ceil(filteredPages.length / recipesPerPage);
+
+
+
+
   return (
     <div className="md:px-[50px] px-[25px]">
       <h1 className="text-4xl text-center mt-[50px]">Recipes</h1>
@@ -25,11 +36,12 @@ const Recipes = () => {
         </div>
         <div>
           <input
+            onChange={e => {setSearch(e.target.value)}}
             type="text"
             placeholder="Search Recipes..."
             className="border border-gray-300 p-2"
           />
-          <button className="bg-blue-500 text-white p-2">Search</button>
+          <button onClick={filteredPages} className="bg-black text-white p-2 hover:bg-gray-700 hover:shadow-sm">Search</button>
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-[25px] justify-items-center mt-[50px]">
