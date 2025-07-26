cb0decb69deb22e01d9dff75905516df17bc1e6a
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-07-26 11:46:17 +0530

    feat: establish MongoDB connection and update dependencies in backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import connectdb from './config/db.js'
 
 const app = express()
 
@@ -6,5 +7,7 @@ app.use(express.json())
 
 
 app.listen(3000, () => {
+    connectdb()
     console.log('Server is running on port 3000')
 })
+
