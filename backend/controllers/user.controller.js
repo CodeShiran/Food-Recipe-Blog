f1fd48faabbaa407eaa7d2b8808246967dbec141
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-07-27 20:13:39 +0530

    feat: add deleteUser endpoint for user deletion functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -55,3 +55,16 @@ export const EditUser = async (req, res) => {
     }
 
 }
+
+export const deleteUser = async (req, res) => {
+    const {id} = req.params
+
+    try {
+        const deletedUser = await User.findByIdAndDelete(id)
+        if(!deletedUser) return res.status(404).json({message: 'User not found'})
+        res.status(200).json({ message: 'User deleted successfully' })
+    } catch (error) {
+        console.error("Error deleting user:", error.message);
+        res.status(500).json({error: 'Internal server error'});
+    }
+}
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { EditUser, getAllUsers, userRegister } from "../controllers/user.controller.js";
+import { deleteUser, EditUser, getAllUsers, userRegister } from "../controllers/user.controller.js";
 
 const userRouter = express.Router()
 
 userRouter.post('/register', userRegister)
 userRouter.get('/', getAllUsers)
 userRouter.put('/:id', EditUser)
+userRouter.delete('/:id', deleteUser)
 export default userRouter;
