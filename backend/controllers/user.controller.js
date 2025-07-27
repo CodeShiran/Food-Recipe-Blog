59c00f3e473c3e2f80f7d12e78c128940f36f73c
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-07-27 19:37:24 +0530

    feat: add EditUser endpoint for updating user details

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -30,5 +30,28 @@ export const getAllUsers = async (req, res) => {
         console.error("Error fetching users:", error.message);
         res.status(500).json({error: 'Internal server error'});
     }
+}
+
+export const EditUser = async (req, res) => {
+    const {id} = req.params
+    const {firstName, lastName, email, image} = req.body
+
+    if(!firstName && !lastName && !email && !image) {
+        return res.status(400).json({message: 'please fill all fields'})
+    }
+
+    const updatedFields = {}
+    if(firstName) updatedFields.firstName = firstName
+    if(lastName) updatedFields.lastName = lastName
+    if(email) updatedFields.email = email
+    if(image) updatedFields.image = image
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(id, updatedFields, { new: true })
+        res.status(200).json({ message: 'User updated successfully', data: updatedUser })
+    } catch (error) {
+        console.error("Error updating user:", error.message);
+        res.status(500).json({error: 'Internal server error'});
+    }
 
 }
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getAllUsers, userRegister } from "../controllers/user.controller.js";
+import { EditUser, getAllUsers, userRegister } from "../controllers/user.controller.js";
 
 const userRouter = express.Router()
 
 userRouter.post('/register', userRegister)
 userRouter.get('/', getAllUsers)
-
+userRouter.put('/:id', EditUser)
 export default userRouter;
