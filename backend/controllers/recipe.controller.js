b654c2ba76b624a259d646e475bc0cb9767c15c5
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-07-28 17:11:02 +0530

    feat: add createRecipe functionality and initialize recipe routes

---
+++ backend/controllers/recipe.controller.js
@@ -0,0 +1,29 @@
+import Recipe from "../models/recipe.js";
+
+const createRecipe = async (req, res) => {
+    const {title, description, content, nutritionalInfo, prepTime, cookTime, ingredients, directions, image, author} = req.body;
+    if (!title || !description || !content || !nutritionalInfo || !prepTime || !cookTime || !ingredients || !directions) {
+        return res.status(400).json({message: 'Please fill all fields'});
+    }
+
+    try {
+        const newRecipe = new Recipe({
+            title,
+            description,
+            content,
+            nutritionalInfo,
+            prepTime,
+            cookTime,
+            ingredients,
+            directions,
+            image,
+            author
+        });
+
+        await newRecipe.save();
+        res.status(201).json({message: 'Recipe created successfully', recipe: newRecipe});
+    } catch (error) {
+        console.error("Error creating recipe:", error.message);
+        res.status(500).json({error: 'Internal server error'});
+    }
+}
