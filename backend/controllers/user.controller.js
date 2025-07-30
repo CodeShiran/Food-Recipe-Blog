b124863643ede0829306603d4c2ccc41ca315d97
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-07-30 14:22:53 +0530

    feat: implement user login functionality and update user routes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.js"
+import jwt from 'jsonwebtoken'
 
 export const userRegister = async (req, res) => {
     const {firstName, lastName, email, password, image} = req.body
@@ -20,6 +21,41 @@ export const userRegister = async (req, res) => {
         console.error("Error during user registration:", error.message);
         res.status(500).json({error: 'Internal server error'});
     }
+}
+
+export const loginUser = async (req, res) => {
+    const {email, password} = req.body
+
+    if(!email || !password) {
+        return res.status(400).json({message: 'Please fill all fields'})
+    }
+
+    try {
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' })
+        }
+
+        const isMatch = await user.comparePassword(password)
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid credentials' })
+        }
+
+        const token = jwt.sign({
+            id: user._id,
+            email: user.email
+        }, process.env.JWT_SECRET, { expiresIn: '1h' })
+
+        res.status(200).json({ message: 'User logged in successfully', data: { id: user._id, email: user.email, token } })
+    } catch (error) {
+        console.error("Error during user login:", error.message);
+        res.status(500).json({error: 'Internal server error'});
+    }
+}
+
+
+export const logoutUser = async (req, res) => {
+    res.status(200).json({ message: "User logged out successfully" });
 }
 
 export const getAllUsers = async (req, res) => {
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { deleteUser, EditUser, getAllUsers, userRegister } from "../controllers/user.controller.js";
+import { deleteUser, EditUser, getAllUsers, loginUser, logoutUser, userRegister } from "../controllers/user.controller.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const userRouter = express.Router()
 
 userRouter.post('/register', authMiddleware, userRegister)
+userRouter.post('/login', loginUser)
+userRouter.post('/logout', authMiddleware, logoutUser)
 userRouter.get('/', authMiddleware, getAllUsers)
 userRouter.put('/:id', authMiddleware, EditUser)
 userRouter.delete('/:id', authMiddleware, deleteUser)
