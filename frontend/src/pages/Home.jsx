8d064f712f470e5acfa440127f985b91413b1327
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-08-15 16:05:57 +0530

    fix: correct import path for Categories component in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "../section/Hero";
-import Categories from "../section/categories";
+import Categories from "../section/Categories";
 import Recipes from "../section/Recipes";
 import BeChef from "../section/BeChef";
 import Instagram from "../section/Instagram";
